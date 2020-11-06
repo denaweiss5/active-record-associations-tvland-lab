b05de9502512b6ca2fd0f40e6b81c477d3d72350
https://learn.co/DenaNeuman <denaweiss5@gmail.com>
2020-11-06 15:18:03 -0600

    commit tree 5e14d7348ccd7bb01e4dca60c075c9bb08cd3432

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
+
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters 
+
+    def actors_list
+        my_actors = self.actors
+        my_actors.map do |actor|
+            actor.full_name
+        end
+    end
   
 end
